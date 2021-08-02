cce25f01aef57cf09a14caedbb3eea092b58ce80
Elvira <57684716+Elvira654@users.noreply.github.com>
2021-08-02 14:35:13 +0200

    Add files via upload

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//Za tri učitana broja ispitati da li je jedan od njih jednak sumi druga dva broja.
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double prvi, drugi, treci;
+            Console.WriteLine("Unesite prvi broj: ");
+            prvi = double.Parse(Console.ReadLine());
+            Console.WriteLine("Unesite drugi broj: ");
+            drugi = double.Parse(Console.ReadLine());
+            Console.WriteLine("Unesite treci broj: ");
+            treci = double.Parse(Console.ReadLine());
+            if (prvi == (drugi + treci))
+            Console.WriteLine("Prvi broj jednak je zbiru drugog i treceg.");
+            if (drugi == (prvi + treci))
+            Console.WriteLine("Drugi broj jednak je zbiru prvog i treceg.");
+            if (treci == (prvi + drugi))
+            Console.WriteLine("Prvi broj jednak je zbiru prvog i drugog.");
+            Console.ReadKey();
+        }
+    }
+}
